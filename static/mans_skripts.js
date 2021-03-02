8955889e7c533b5fb2127ab33facf26e28080242
Kristaps <esmitskristaps@inbox.lv>
2021-03-02 14:13:55 +0200

    Izmainas

---
+++ static/mans_skripts.js
@@ -0,0 +1,9 @@
+console.log("Viss strādā!")
+alert("Hello!")
+//alert("Kristaps!")
+
+let mainigaisTeksts = "skola"
+let mainigaisSkaitlis = 7
+
+console.log(mainigaisTeksts)
+console.log(mainigaisSkaitlis)
